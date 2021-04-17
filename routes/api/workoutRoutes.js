460701347198f010cd296c45f7c93edf27a0800c
rsco <RaquelLee@users.noreply.github.com>
2021-04-17 15:11:19 -0400

    workout api routes

---
+++ routes/api/workoutRoutes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+// let Workout = require("../../models/workout");
+let db = require("../../models");
+
+//? pathing and adding file name
+
+//create workout
+router.post("/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+//update workout
+router.put("/workouts/:id", (req, res) => {
+    db.Workout.updateOne(
+        { _id: req.params },
+        { $push: { exercises: req.body } })
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+//get weeks workouts
+router.get("/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+        { $addFields: { totalDuration: { $sum: 
+            '$exercises.duration', 
+        }, }, },
+    ]).sort({ _id: -1 }).limit(7)
+        .then(data => {
+            res.json(data)        })
+                .catch(err => {
+                    res.json(err);
+                });
+
+});
+// get last workout
+router.get("/workouts", (req, res) => {
+    db.Workout.aggregate([
+        { $addFields: { totalDuration: { $sum: 
+            '$exercises.duration', 
+        }, }, }]).then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        });    //grabs last in api.js
+});
+
+module.exports = router;
